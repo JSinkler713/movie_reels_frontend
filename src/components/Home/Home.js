120b5f1ae74fa679b2908ff62262fcf7f7405551
James Sinkler <jamessinkler713@gmail.com>
2020-01-22 12:26:26 -0800

    adds basic description of app

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className='home-page-container'>
+      <h2 className='welcome-header'>Welcome to Movie_Reels</h2>
+      <p>Movie_Reel is a site dedicated to the movies you love. When you register as a Movie_Reels user and Login you will have access to start making Reels of your favorite movies. Think of a 'Reel' as you would a 'playlist'. A 'playlist of songs' is a 'Reel of movies'.</p>
+      <p>By making Reels you will not only have a place to save your favorite movies, and make awesome compilations, but also a way to share movies you love with others. Users will have the ability to "Search for reels like ____.". Users enter a movie they love, and then Movie_Reels find Reels that user may like based on the movie.</p>
+      <p>The idea is to help our users move from a favorite movie, to a new Reel with potentially many more <strong>new</strong> favorite movies. Hopefully this app will help you find that next movie to watch, as you sink in your couch with a nice bowl of popcorn. Thanks for being a user and contributing to our community!</p>
     </div>
   );  
 };
