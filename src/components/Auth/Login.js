60b199c9be94b4d4438a8d58c7965481001ea301
James Sinkler <jamessinkler713@gmail.com>
2020-09-07 18:46:15 -0700

    refactors to use hooks

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useContext} from 'react';
 import { API_URL } from '../../constants/constants';
 
-class Login extends Component {
-  state = {
-    email: '',
-    password: '',
-    error: null,
+const Login = (props)=>{
+  //state = {
+    //email: '',
+    //password: '',
+    //error: null,
+  //};
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleChange = (event) => {
+    if (event.target.name == 'password'){
+      setPassword(event.target.value)
+    } else if (event.target.name == 'email') {
+      setEmail(event.target.value)
+    }
   };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+//  handleChange = (event) => {
+//    this.setState({
+//      [event.target.name]: event.target.value,
+//    });
+//  };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     // handle submit here
     event.preventDefault();
-    const user = this.state
+ //   const user = this.state
+    const user = {
+      email: email,
+      password: password,
+      error: null
+    }
     fetch(`${API_URL}/login`, {
       method: "POST",
       headers: {
@@ -28,36 +45,38 @@ class Login extends Component {
       .then(res=> res.json())
       .then(data=> {
         console.log("Success from Auth/login", data);
-        this.props.setCurrentUser(data);
+        //
+        props.setCurrentUser(data);
        // sends user to reels page
-        this.props.history.push('/reels');
+        props.history.push('/reels');
       })
       .catch(err => {
-        this.setState({error: err.message })
+      //  this.setState({error: err.message })
+        setError(err.message)
       });     
 
   };
 
-  render() {
+  //render() {
     return (
       <div className="login-container">
-        {this.state.error && (
+        {error && (
           <div className="alert alert-danger alert-dismissible fade show" style={{width: '100%'}} role="alert">
-            {this.state.error}
+            {error}
             <button type="button" className="close" data-dismiss="alert" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
         )}
         <section id="login" className="col-md-6 offset-md-3">
-          <form  className="login-form" onSubmit={this.handleSubmit}>
+          <form  className="login-form" onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="email" className='email-login'>Email </label>
-              <input type="email" className='login-input' id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+              <input type="email" className='login-input' id="email" name="email" value={email} onChange={handleChange} />
             </div>
             <div className="form-group">
               <label htmlFor="email" className='password-login'>Password </label>
-              <input type="password" id="password" className='login-input' name="password" value={this.state.password} onChange={this.handleChange} />
+              <input type="password" id="password" className='login-input' name="password" value={password} onChange={handleChange} />
             </div>
             <div className="login-button-container">
                 <button type="submit" className="login-button">Login</button>
@@ -66,7 +85,7 @@ class Login extends Component {
         </section>
       </div>
     );
-  };
+  //};
 };
 
 export default Login;
