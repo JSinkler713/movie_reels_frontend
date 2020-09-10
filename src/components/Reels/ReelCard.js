f93ddbd3415cd80383a670279976978da2e2a996
James Sinkler <jamessinkler713@gmail.com>
2020-09-09 21:49:10 -0700

    sets logic to functional state; randomizes image from array of movies in reel

--- src/components/Reels/ReelCard.js
+++ src/components/Reels/ReelCard.js
@@ -1,15 +1,51 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {UserContext} from '../../UserContext'
 import { Link } from 'react-router-dom'
 import { API_URL } from '../../constants/constants';
 
 //when using a functional component we need to manually pass down the props to that component
 
 
-class ReelCard extends Component {
-  state = {
-    movies: []
+const ReelCard = (props)=> {
+  const [movies, setMovies] = useState([])
+  const [image, setImage] = useState('')
+  const [user, setUser] = useContext(UserContext)
+
+  const setReelId =()=> {
+    setUser({
+      ...user,
+      reel_id: props.reel_id
+    })
+    
   }
-  
+
+  useEffect(()=> {
+    fetch(`${API_URL}/reels/${props.reel_id}/movies`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res=> res.json())
+      .then(data=> {
+        console.log("Success we got the data", data);
+        if (data.length > 0) {
+          const length = data.length
+          let random = Math.floor(Math.random() * length)
+          setMovies(data[random])
+          setImage(data[random].Poster)
+        }else {
+          setImage('https://placebear.com/g/200/300')
+        }
+       // sends user to profile page
+       // this.props.history.push('/reels');
+      })
+      .catch(err => {
+        //this.setState({error: err.message })
+      }); 
+  }, [])
+
+  /*
   componentDidMount() {
     fetch(`${API_URL}/reels/${this.props.reel_id}/movies`, {
       method: "GET",
@@ -32,30 +68,22 @@ class ReelCard extends Component {
     let id = this.props.reel_id
     this.props.deleteReel(id)
   }
-  
-  
-  render() {
-    if (this.state.movies.length == 0) {
-      return ( 
-      <div className='reel-card'>
-        <Link onClick={this.props.fetchMovies} to={`/reels/${this.props.reel_id}`}>
-          { this.props.reelTitle } 
-        </Link><br/>
-        <div className='poster-standin'>
-        </div>
-        <button className='delete-reel-button' onClick={this.deleteReel}>Delete Reel </button>
-      </div>
-    )} else {
-    return ( 
-      <div className='reel-card'> 
-        <Link onClick={this.props.fetchMovies} to={`/reels/${this.props.reel_id}`}>
-          { this.props.reelTitle } 
-        </Link><br/>
-           <img src={this.state.movies[0].Poster} alt='no movies yet' />
-        <button className='delete-reel-button' onClick={this.deleteReel}>Delete Reel </button>
-      </div>
-      )}
-   }
+  */  
+  return ( 
+    <div className='reel-card'>
+      <Link onClick={setReelId} to={`/reels/${props.reel_id}`}>
+        { props.reelTitle } 
+      </Link><br/>
+        {image 
+          ? (<img src={image} alt='no movies yet' />)
+          : (
+          <div className='poster-standin'>
+          </div>
+          )
+        }
+      <button className='delete-reel-button' onClick={()=> console.log('delete')}>Delete Reel </button>
+    </div>
+  )
 };
 
 export default ReelCard;
