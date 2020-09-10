9cb96c8c24829d8ef765e3cb3e0ce999d5b51efe
James Sinkler <jamessinkler713@gmail.com>
2020-09-09 21:44:07 -0700

    changes up some functionality in reels

--- src/components/Reels/Reel.js
+++ src/components/Reels/Reel.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import { API_URL } from '../../constants/constants';
+import {UserContext} from '../../UserContext'
 import MovieContainer from '../../containers/MovieContainer'
 
-class Reel extends Component {
+function Reel() {
+  const [foundMovies, setFoundMovies] = useState([])
+  const [reelMovies, setReelMovies] = useState([])
+  const [user, setUser] = useContext(UserContext)
+  const [movieToSearch, setMovieToSearch] = useState('')
+  const [newSearch, setNewSearch] = useState('false')
+
+
+  useEffect(()=> {
+    console.log('user info', user)
+    fetchMovies(user.reel_id)
+  },[])
+
+  /* 
   state = { 
     movies: '', 
     reel_id:'',
@@ -10,30 +24,29 @@ class Reel extends Component {
     inputText: '',
     searchedMovies: '',
     addMovie: false
-  }
+  } */
   
-  handleInput = (e) => {
-    this.setState({inputText: e.target.value})
+  const handleInput = (e) => {
+    setMovieToSearch(e.target.value)
   }
 
-  handleSearch = (e) => { 
-    let url='http://www.omdbapi.com/?apikey=3a55a9e6&s='+this.state.inputText
+  const handleSearch = (e) => { 
+    let url='http://www.omdbapi.com/?apikey=3a55a9e6&s='+ movieToSearch
     fetch(url, {method: 'get'})
       .then(response=> response.json())
       .then(body=> {
-        this.setState({searchedMovies: body.Search})
-        console.log(this.state.movies)
+        setFoundMovies(body.Search)
       })
   }
   
-  searchAvailable = (e) => {
+/*   searchAvailable = (e) => {
     this.setState({addMovie: true})
   }
   searchOff = () => {
     this.setState({addMovie: false})
-  }
+  } */
 
-  fetchMovies = (reel_id) => {
+  const fetchMovies = (reel_id) => {
     console.log(`fetching all movies with reel id ${reel_id}`)
     console.log("if this all works then I need to write my fetch request")
     fetch(`${API_URL}/reels/${reel_id}/movies`, {
@@ -45,26 +58,58 @@ class Reel extends Component {
       .then(res=> res.json())
       .then(data=> {
         console.log("Success we got the movie data", data);
-        this.setState({movies: (data)})
-        this.setState({reelName: (data[0].Reel)})
+        setReelMovies(data)
+        //this.setState({movies: (data)})
+        //this.setState({reelName: (data[0].Reel)})
        // sends user to profile page
        // this.props.history.push('/reels');
       })
       .catch(err => {
-        this.setState({error: err.message })
+        //this.setState({error: err.message })
       }); 
   }
 
-  componentDidMount() {
+  /* componentDidMount() {
     // console.log({ this.props.match.params.id })
     this.setState({ reel_id: this.props.match.params.id})
     this.fetchMovies(this.props.match.params.id)
 
-  }
+  } */
 
-  render() {
+  let arrayOfMovies= reelMovies.map((movie, i)=> {
+    return <div className='myMovies'>
+      <h3>Title:{movie.Movie}</h3>
+      <img src={movie.Poster} alt='incorrect image' />
+      </div>
+  })  
 
-  
+  //render() {
+    return (
+      <div>
+      <div className="Search">
+          <p>Movie Search</p>  
+    <input
+      onChange={handleInput}
+      value={movieToSearch}
+    />
+    <button onClick={handleSearch}>click here</button>
+    <div className="myMoviesContainer">
+       {
+//       this.state.searchedMovies.length === 0
+       (foundMovies.length === 0 && newSearch)
+       ? "Search for your movies to add"
+       : <MovieContainer reel_id={user.reel_Id} setNewSearch={()=>setNewSearch(!newSearch)} movies={foundMovies} />
+       }
+      </div>
+      </div>
+      <h2>My Movies</h2>
+      <div className='myMoviesContainer'>
+        {arrayOfMovies}
+      </div>
+      </div>
+    )
+
+  /* 
     if (this.state.movies.length !== 0 || this.state.addMovie === true) {
         return ( 
           <div className="Search">
@@ -99,8 +144,7 @@ class Reel extends Component {
             </div>
           </div>
       );
-    }
-  }
+    } */
 };
 
 export default Reel;
