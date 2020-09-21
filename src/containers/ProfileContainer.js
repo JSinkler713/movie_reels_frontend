d47fa99bf8f631c94d71369d6fd3e29c7e4ae8a4
James Sinkler <jamessinkler713@gmail.com>
2020-09-20 20:24:29 -0700

    removes old way to mount Profile

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -5,23 +5,6 @@ import { API_URL } from '../constants/constants';
 
 function ProfileContainer() {
   const [user, setUser] = useContext(UserContext)
-
-  /*
-  componentDidMount() {
-    console.log(this.props)
-    console.log('props in profile container')
-    // Fetch call to get user information
-    fetch(`${API_URL}/users`, {
-      headers: {
-        authorization: `Bearer ${localStorage.uid}`
-      }   
-    })  
-    .then(res => res.json())
-//    .then(data => this.setState({ user: data.data })) 
-    .then(data=> console.log(data))
-    .catch(err => console.log(err))
-  };  
-  */
   return <Profile user={user} />
 };
 
